71e77c71a6b0eda71732ba6d4dee3e15bfd0e960
ederson <edersonsgoncalves@gmail.com>
2025-07-13 21:53:59 -0300

    feat: adicao dieta classe Animal e método comer()

--- planeta_py.py
+++ planeta_py.py
@@ -5,7 +5,7 @@ class FormaDeVida:
         self.nivel_energia = nivel_energia
 
     def __str__(self) -> str:
-        return f""
+        return f"Nome: {self.nome}\nEspécie: {self.especie}\nNivel de Energia: {self.nivel_energia}"
     
 class Planta (FormaDeVida):
     def __init__(self, nome: str, especie: str, nivel_energia: float):
@@ -14,5 +14,17 @@ class Planta (FormaDeVida):
     def fotossintese(self):
         self.nivel_energia = 100
 class Animal (FormaDeVida):
-    def __init__(self, nome: str, especie: str, nivel_energia: float):
+    def __init__(self, nome: str, especie: str, nivel_energia: float, dieta:str):
         super().__init__(nome, especie, nivel_energia)
+        self.dieta = dieta
+    
+    def __str__(self) -> str:
+        return super().__str__() + f"\nDieta: {self.dieta}"
+    
+    def comer(self, alimento):
+        self.alimento = alimento
+
+
+animal1 = Animal(nome="Lobo", especie="Selvagem",nivel_energia=90.0, dieta="Carnívoro")
+
+print (animal1)
